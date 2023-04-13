3696dc6c62574aa5f1fb53802706a6ddc8f48e7a
Kauavalladares <kaua.valldares@gmail.com>
2023-04-13 14:28:42 -0300

    adição de melhorias e funções, o codigo esta completo o exercico foi finalizado, porem ainda espaço para otimização de codigo futuras.

--- maquina-de-cafe/src/MaquinaDeCafe.java
+++ maquina-de-cafe/src/MaquinaDeCafe.java
@@ -40,143 +40,98 @@
 //               2 moedas de 10
 //______________________________________________________________________________________________________________________
 
+import java.util.Arrays;
 import java.util.Scanner;
-
 import static java.lang.System.*;
 
-public  class MaquinaDeCafe extends DevolveTroco {
+public class MaquinaDeCafe extends DevolveTroco {
 
 
-    public boolean checaProdutovalido(String codproduto){
-       codproduto  = codproduto.toUpperCase();
-
-
-        return false;
-    }
-    public static String selecionaProduto(String s) {
-         s = s.toUpperCase();
-         switch (s) {
-             case "A50" -> {
-                 out.println(CardapioEnum.CAPPUCCINO.opcaoeEscolhida());
-                 s = CardapioEnum.CAPPUCCINO.getNome();
-             }
-             case "B40" -> {
-                 out.println(CardapioEnum.VANILLA.opcaoeEscolhida());
-                 s = CardapioEnum.VANILLA.getNome();
-             }
-             case "A60" -> {
-                 out.println(CardapioEnum.MOCACCINO.opcaoeEscolhida());
-                 s = CardapioEnum.MOCACCINO.getNome();
-             }
-             case "M20" -> {
-                 out.println(CardapioEnum.CARIOCA.opcaoeEscolhida());
-                 s = CardapioEnum.CARIOCA.getNome();
-             }
-             case "C10" -> {
-                 out.println(CardapioEnum.CHOCOLATE_QUENTE.opcaoeEscolhida());
-                 s = CardapioEnum.CHOCOLATE_QUENTE.getNome();
-             }
-             case "0" -> {
-                 out.println("Finalizando ...");
-                 exit(0);
-             }
-             default -> {
-                 out.println("Código de produto inválido. \n Digite novamente ou digite 0 para sair.  ");
-                 Scanner sc = new Scanner(in);
-                 s = sc.next();
-                 selecionaProduto(s);
-                 produto(s);
-             }
-         }
-
-
-        return s;
+    public static CardapioEnum selecionaProduto(String s) {
+        s = s.toUpperCase();
+        CardapioEnum resultado = null;
+        switch (s) {
+            case "A50" -> resultado = CardapioEnum.CAPPUCCINO;
+            case "B40" -> resultado = CardapioEnum.VANILLA;
+            case "A60" -> resultado = CardapioEnum.MOCACCINO;
+            case "M20" -> resultado = CardapioEnum.CARIOCA;
+            case "C10" -> resultado = CardapioEnum.CHOCOLATE_QUENTE;
+        }
+        out.println("A bebida escolhida foi:" + resultado.getNome());
+        return resultado;
     }
 
-     public static double produto(String codproduto) {
-         String prod = codproduto.toUpperCase();
-         double valor = 0;
+    private static boolean validaTexto(String codProduto) {
+    /*   List<String> codigos = new ArrayList<>();
 
-         if (prod.equals(CardapioEnum.CAPPUCCINO.getCodigo())) {
-             valor = 4.50;
-             return valor;
-         }
+       codigos.add("A50");
+       codigos.add("B40");
+       codigos.add("A60");
+       codigos.add("M20");
+       codigos.add("C10");
 
-         if (prod.equals(CardapioEnum.VANILLA.getCodigo())) {
-             valor = 5.00;
-             return valor;
-         }
-
-         if (prod.equals (CardapioEnum.MOCACCINO.getCodigo())) {
-             valor = 3.75;
-             return valor;
-         }
-
-         if (prod.equals (CardapioEnum.CARIOCA.getCodigo())) {
-             valor = 2.20;
-             return valor;
-         }
-
-         if (prod.equals (CardapioEnum.CHOCOLATE_QUENTE.getCodigo())) {
-             valor = 8.40;
-             return valor;
-         }
-
-         return valor;
-     }
+       return codigos.contains(codProduto.toUpperCase());
+      */
+        boolean resultado = !Arrays.asList("A50", "B40", "A60", "M20", "C10").contains(codProduto.toUpperCase());
+        if (resultado){
+            out.println("Código de produto inválido. ");
+        }
+        return resultado;
+    }
 
 
+    public static void MenuPrincipal() {
 
 
- public static void MenuPrincipal() {
+        out.println("Escolha a bebida de sua preferência: \n ");
+
+        out.println("____________________________________________________________________________");
+
+        out.println("| Código Produto |   Valor   |             Produto                         |");
+
+        out.println(CardapioEnum.CAPPUCCINO.opacaoCardapio() + "       |");
+
+        out.println(CardapioEnum.VANILLA.opacaoCardapio() + "         |");
+
+        out.println(CardapioEnum.MOCACCINO.opacaoCardapio() + "       |");
+
+        out.println(CardapioEnum.CARIOCA.opacaoCardapio() + "         |");
+
+        out.println(CardapioEnum.CHOCOLATE_QUENTE.opacaoCardapio() + "|");
+
+        out.println("|__________________________________________________________________________| \n");
+
+    }
 
 
-      out.println("Escolha a bebida de sua preferência: \n ");
-
-      out.println("____________________________________________________________________________");
-
-      out.println("| Código Produto |   Valor   |             Produto                         |");
-
-      out.println(CardapioEnum.CAPPUCCINO.opacaoCardapio() +"       |");
-
-      out.println(CardapioEnum.VANILLA.opacaoCardapio()+"         |");
-
-      out.println(CardapioEnum.MOCACCINO.opacaoCardapio()+"       |");
-
-      out.println(CardapioEnum.CARIOCA.opacaoCardapio()+"         |");
-
-      out.println(CardapioEnum.CHOCOLATE_QUENTE.opacaoCardapio()+"|");
-
-      out.println("|__________________________________________________________________________| \n");
-
-     }
+    public static void main(String[] args) {
 
 
+        MenuPrincipal();
+        Scanner sc = new Scanner(in);
+        String codigodoproduto;
 
-     public static void main(String[] args) {
+        do {
+            out.println("Digite codigo do produto: ");
+            codigodoproduto = sc.nextLine();
+        } while (validaTexto(codigodoproduto));
+
+        CardapioEnum bebida = selecionaProduto(codigodoproduto);
+
+        double valorInserido;
+        do {
+            out.println("Insira o valor da bebida: " + bebida.getValor());
+            valorInserido = sc.nextDouble();
+        } while (valorInserido < bebida.getValor());
 
 
-         MenuPrincipal();
-         Scanner sc = new Scanner(in);
-         out.println("Digite codigo do produto: ");
-         String Codigodoproduto = sc.next();
 
-         String bebida = selecionaProduto(Codigodoproduto);
-         double preco = produto(bebida);
-
-
-         Scanner scv = new Scanner(in);
-         double valorInserido = scv.nextDouble();
-
-        out.println(calculaTroco(preco,valorInserido));
+        out.println(calculaTroco(bebida.getValor(), valorInserido));
         out.println("Sua está bebida sendo preparada aguarde! \n");
         out.println("aqui está sua bebida: " + bebida + " ☕︎");
 
 
-
-
-
-
-     }
+    }
 }
+
 
